3b21edb71b1bd6e7bd06e738aecf4a88b90d421a
杭 <1942499981@qq.com>
2023-06-02 01:19:04 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: 杭
+ * @Date: 2023-06-02 01:05:05
+ * @LastEditors: 杭
+ * @LastEditTime: 2023-06-02 01:05:16
+ */
+module.exports = {
+    publicPath: "./",
+};
